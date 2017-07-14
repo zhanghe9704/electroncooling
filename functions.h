24de0fc538c7944b0ecee6894ba7e1f91fb04711
hezhang <zhanghe1979@gmail.com>
2017-07-14 18:01:12 -0400

    remove old files

--- functions.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-
-int gaussian_random(unsigned int n, double *random_num, double sigma=1, double avg=0);
-int uniform_random(unsigned int n, double *random_num, double r_min, double r_max);
-int gaussian_random_adjust(unsigned int n, double *random_num, double sigma, double avg=0);
-int uniform_random_adjust(unsigned int n, double *random_num, double avg=0);
-#endif // FUNCTIONS_H
